896a24ffa58d2680d67ecf8f8efb3330cd2d3d3b
Matej Ramuta <matej.ramuta@gmail.com>
2018-12-21 14:32:38 -0500

    package name in init

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
-from setuptools import setup
+import setuptools
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-setup(
+setuptools.setup(
     name='tifico',
     url='https://github.com/smartninja/tifico',
     author='Matej Ramuta',
